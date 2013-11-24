add2fc50da6f539aef8c8a6d63a7ca73f4b67abe
tomdov <tomdovdev@gmail.com>
2013-11-25 00:24:13 +0200

    remove check of following/followers

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -33,14 +33,6 @@ describe PagesController do
         other_user = FactoryGirl.create(:user, :email => FactoryGirl.generate(:email))
         other_user.follow!(@user)
       end
-
-      it "should have the right follower/following counts" do
-        get :home
-        response.should have_selector('a', :href => following_user_path(@user),
-                                           :content => "0 following")
-        response.should have_selector('a', :href => followers_user_path(@user),
-                                      :content => "1 follower")
-      end
     end
   end
 
